2778b982cf975583d72729fa0cb1d332007d6699
Payton Wallace <payton@paytonwallace.com>
2026-03-23 09:08:43 -0500

    fix: redirect /home-page and /home to /

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,20 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "logo.clearbit.com" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/home-page",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
